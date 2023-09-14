91440f23044bbda7579a82b6027d61d15369cc2a
soumyamothe <soumyamothe@appincubator.io>
2023-09-14 18:27:06 +0530

    modified displaying correct answer

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -9,9 +9,9 @@ const Answer = ({
 }) => {
   const label = ["A", "B", "C", "D"];
   const isSelected =   selectedAnswer === answerText;
-  const isCorrect = isSelected &&   selectedAnswer === correctAnswer;
+  const isCorrect = isSelected &&  selectedAnswer === correctAnswer;
   const isWrong = isSelected && !isCorrect;
-  const isWrongSelectedAnswer = isSelected && !isCorrect;
+
 
   const answerClass = `answer ${isSelected ? "selected-answer" : ""} ${
     isCorrect ? "correct-answer" : ""
@@ -22,7 +22,7 @@ const Answer = ({
     <div className={answerClass} onClick={() => onSelectAnswer(answerText)}>
       <div className="answer-label">{label[index]}</div>
       <div className="answer-text">{answerText}</div>
-      {isWrongSelectedAnswer && (
+      {isWrong && (
         <div className="correct-answer-indicator">
           Correct Answer: {correctAnswer}
         </div>
